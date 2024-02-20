3baa15574f50292ca339b15e8bda878d4de81a1c
Craner <iayfnp@gmail.com>
2024-02-20 18:04:31 -0500

    Update

--- ASTRO_7N.py
+++ ASTRO_7N.py
@@ -1,16 +1,12 @@
-import time
-import keyboard
 from PIL import Image
+import keyboard
 import pyautogui as gui
+import time
 
 origin = time.time()
-gui.PAUSE = 0.01
+gui.PAUSE = 0.025
 i = 0
-while not keyboard.is_pressed('ESC+SHIFT') and time.time() - origin < 1800:
-    try:
-        gui.locateOnScreen('copper.png', region=(2054, 892, 95, 49), confidence=0.9)
-        break
-    except:
-        if keyboard.is_pressed('SHIFT+CTRL'):
-            gui.press(str(i := i % 5 + 1))
-            gui.press('ENTER')
+while not keyboard.is_pressed('ESC+`') and time.time() - origin < 1800:
+    if keyboard.is_pressed('SHIFT+CTRL') and not gui.pixelMatchesColor(2150, 955, (157, 159, 129), tolerance=10):
+        gui.press(str(i := i % 5 + 1))
+        gui.press('ENTER')
