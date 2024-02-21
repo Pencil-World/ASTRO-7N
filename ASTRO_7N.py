77d321554b66b99699977599bc23d1d158e7a1b6
Craner <iayfnp@gmail.com>
2024-02-21 01:50:25 -0500

    wasup

--- ASTRO_7N.py
+++ ASTRO_7N.py
@@ -1,4 +1,3 @@
-from PIL import Image
 import keyboard
 import pyautogui as gui
 import time
---
+++ ASTRO_7N_slow.py
@@ -0,0 +1,11 @@
+import keyboard
+import pyautogui as gui
+import time
+
+origin = time.time()
+gui.PAUSE = 0.1
+i = 0
+while not keyboard.is_pressed('RIGHT SHIFT+TAB') and time.time() - origin < 1800:
+    if keyboard.is_pressed('RIGHT SHIFT'):
+        gui.press(str(i := i % 5 + 1))
+        gui.press('ENTER')
