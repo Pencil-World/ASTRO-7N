93ca2af752c63a4e29d9748a6f7ed14e16a398e9
Craner <iayfnp@gmail.com>
2024-04-03 16:30:50 -0400

    First test full test run of CopperQuizzes

--- ASTRO_7N.py
+++ ASTRO_7N.py
@@ -25,11 +25,21 @@ def clean_extracted_text(extracted_text):
         text_block[-1] += remove_punctuation(bounding_box[1]) + " "
     return text_block
 
+def get_best_match(r, value):
+    similarities = [[SequenceMatcher(None, value, elem).ratio(), elem] for elem in r]
+    best_match = max(similarities, key=lambda elem: elem[0])
+    if best_match[0] < 0.875:
+        global CopperQuizzes
+        CopperQuizzes = False
+        print(f"ERROR: The string [{best_match[1]}] is not similar enough to the string [{value}]. ")
+        return 0
+    return best_match
+
 CopperQuizzes = input("Do you want the program to complete copper quizzes as well as advance the dialogue? [Y/N] ") == 'Y'
 info = setup(3 if CopperQuizzes == False else 5)
 x, y, expectedRGBColor = info["detection_point"]
 if CopperQuizzes:
-    dialogue_box = [info["dialogue_box"][0][0], info["dialogue_box"][0][1], info["dialogue_box"][0], info["dialogue_box"][0]]
+    dialogue_box = info["dialogue_box"]
     quizlet = info["quizlet"]
 
 origin = time.time()
@@ -41,20 +51,10 @@ while not keyboard.is_pressed('ESC+`') and time.time() - origin < 1800:
             gui.press(str(i := i % 5 + 1))
             gui.press('ENTER')
         elif CopperQuizzes:
-            keyboard.wait('ENTER')
-            keyboard.wait('SHIFT+CTRL')
-
             extracted_text = extract_screen_text()
             text_block = clean_extracted_text(extracted_text)
 
-            # similarities = [[SequenceMatcher(None, text_block[0], question).ratio(), question, answer] for question, answer in quizlet.items()]
-            # best_match = max(similarities, key=lambda elem: elem[0])
-            # if best_match[0] < 0.875:
-            #     CopperQuizzes = False
-            #     print(f"ERROR: The observed string [{text}] from the application is not similar enough to the given string [{best_match[1]}] from the imported quizlet. ")
-            #     continue
-
-            # moara = [[SequenceMatcher(None, answer, best_match[2]).ratio(), answer] for answer in text]
-            # best_match = max(moara, key=lambda elem: elem[0])
-            # gui.press(str(best_match))
-            # gui.press('ENTER')
+            best_match_question = get_best_match(quizlet.keys(), text_block[0])
+            best_match_answer = get_best_match(text_block, quizlet[best_match_question[1]])
+            gui.press(str(text_block.index(best_match_answer[1])))
+            gui.press('ENTER')
